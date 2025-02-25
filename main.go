3f39ba06fa9741ab77b34a3694fde75383f096d7
Vitaly Kochetov <kochetovit@gmail.com>
2025-02-25 21:24:18 +0300

    remove unused method

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"log/slog"
 
-	"7tv/tg/config"
 	"7tv/tg"
+	"7tv/tg/config"
 )
 
 func main() {
@@ -17,7 +17,7 @@ func main() {
 
 	tgBot := tg.NewBot(cfg)
 
-	if err := tgBot.AddFileStickerToSet("https://7tv.app/emotes/01FFFPWV180007P57XYW0BHF1Z"); err != nil {
+	if err := tgBot.AddStickerToSet("https://7tv.app/emotes/01FFFPWV180007P57XYW0BHF1Z"); err != nil {
 		slog.Error("failed to get sticker set", "err", err.Error())
 	}
 }
--- tg/tgbot.go
+++ tg/tgbot.go
@@ -39,60 +39,7 @@ func (t *Bot) constructURL(path string) string {
 	return t.cfg.TelegramAPIBaseURL + t.cfg.TelegramBotToken + path
 }
 
-func (t *Bot) AddStickerToSet(name string) error {
-	u := t.constructURL("/addStickerToSet")
-
-	reqBody, err := json.Marshal(
-		dto.AddStickerToSetRequest{
-			UserID: t.cfg.UserID,
-			Name:   name,
-			Sticker: dto.InputSticker{
-				Sticker:   t.cfg.Splort,
-				Format:    dto.StickerFormatStatic,
-				EmojiList: []string{"💀"},
-			},
-		},
-	)
-	if err != nil {
-		slog.Error("failed to marshal request body", "err", err.Error())
-
-		return err
-	}
-
-	reader := bytes.NewReader(reqBody)
-
-	req, err := http.NewRequest(http.MethodPost, u, reader)
-	if err != nil {
-		slog.Error("failed to create request", "err", err.Error())
-
-		return err
-	}
-
-	fmt.Println(string(reqBody))
-
-	// TODO: think about it
-	req.Header.Set("Content-Type", "application/json")
-
-	res, err := t.client.Do(req)
-	if err != nil {
-		slog.Error("failed to send request", "err", err.Error())
-
-		return err
-	}
-
-	resBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		slog.Error("failed to read response body", "err", err.Error())
-
-		return err
-	}
-
-	fmt.Printf("%+v", string(resBody))
-
-	return nil
-}
-
-func (t *Bot) AddFileStickerToSet(url string) error {
+func (t *Bot) AddStickerToSet(url string) error {
 	emoteID := path.Base(url)
 
 	tmpInput, err := downloadImage(url7tv + emoteID + pngSuffix)
